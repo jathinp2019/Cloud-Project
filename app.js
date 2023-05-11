188957d0d089812fdfd862752ed1ba9aa463dd93
jathinp2019 <jathinp2019@gmail.com>
2023-05-11 18:55:24 +0530

    intial commit

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('<h1>AA</h1>')
+    res.send('<h1>AA</h1><h2>hai surajs 5 wives</h2>')
 })
 
 app.listen(port,() => {
-    console.log('listening on port')
+    console.log(`listening on port ${port}`)
 })
+
